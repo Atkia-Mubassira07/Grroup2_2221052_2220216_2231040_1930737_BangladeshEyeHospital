1cea671e34fa9cf8e2b0d8af9bc8bbcd913cd720
Atkia-Mubassira <atkiamubassira287@gmail.com>
2024-04-16 04:33:46 +0600

    Project

---
+++ src/main/EditUSerAccountController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+/**
+ * FXML Controller class
+ *
+ * @author atkia
+ */
+public class EditUSerAccountController implements Initializable {
+
+    @FXML
+    private ComboBox<?> myComboBox;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void SaveButton(ActionEvent event) throws IOException{
+        Parent parent;
+        parent = FXMLLoader.load(getClass().getResource("Administrator_Homepage.fxml"));
+       
+        //parent = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(parent);
+        Stage stg = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //   stg.setTitle("Checking... Stage switching");
+
+        stg.setScene(scene);
+        stg.show();
+    }
+    
+}
